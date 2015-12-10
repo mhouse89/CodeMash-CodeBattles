1e21a1de116720d519218f154a0a349074506776
mhouse89 <matthew.house89@gmail.com>
2015-12-10 18:53:37 -0500

    rewrote rspec tests for Roman to arabic conversion.

--- RomanNumeralsKataRuby/spec/romannumerals_spec.rb
+++ RomanNumeralsKataRuby/spec/romannumerals_spec.rb
@@ -7,61 +7,46 @@ describe "Roman Numerals" do
     @roman = RomanNumeral.new
   end
 
-  context "wacky romans didn't do 0" do
-    it "should not convert zero" do
-      @roman.convert(0).should == ""
-    end
-  end
 
-  context "convert base symbols (I, V, X, L, C)" do
-    it "should convert number 1 to numeral I" do
-      @roman.convert(1).should == "I"
-    end
-    it "should convert number 5 to numeral V" do
-      @roman.convert(5).should == "V"
-    end
-    it "should convert number 10 to numeral X" do
-      @roman.convert(10).should == "X"
-    end
-    it "should convert number 50 to numeral L" do
-      @roman.convert(50).should == "L"
-    end
-    it "should convert number 100 to numeral C" do
-      @roman.convert(100).should == "C"
+  context "Converting Roman Numerals to number - " do
+    it "should convert I to 1" do
+      @roman.convert("I").should == 1
     end
 
-  end
-
-  context "convert additions" do
-    it "should convert number 2 to numeral II" do
-      @roman.convert(2).should == "II"
+    it "should convert II to 2" do
+      @roman.convert("II").should == 2
     end
 
-    it "should convert number 3 to numeral III" do
-      @roman.convert(3).should == "III"
+    it "should convert III to 3" do
+      @roman.convert("III").should == 3
     end
 
-    it "should convert number 8 to numeral VIII" do
-      @roman.convert(8).should == "VIII"
-    end
-    it "should convert number 27 to numeral XXVII" do
-      @roman.convert(27).should == "XXVII"
+    it "should convert IV to 4" do
+      @roman.convert("IV").should == 4
     end
 
-  end
+    it "should convert V to 5" do
+      @roman.convert("V").should == 5
+    end
 
-  context "convert subtractions" do
-    it "should convert number 4 to numeral IV" do
-      @roman.convert(4).should == "IV"
+    it "should convert VI to 6" do
+      @roman.convert("VI").should == 6
     end
-    it "should convert number 9 to numeral IX" do
-      @roman.convert(9).should == "IX"
+
+    it "should convert VII to 7" do
+      @roman.convert("VII").should == 7
     end
-    it "should convert number 40 to numeral XL" do
-      @roman.convert(40).should == "XL"
+
+    it "should convert VIII to 8" do
+      @roman.convert("VIII").should == 8
     end
-    it "should convert number 44 to numeral XLIV" do
-      @roman.convert(44).should == "XLIV"
+
+    it "should convert IX to 9" do
+      @roman.convert("IX").should == 9
+    end
+
+    it "should convert X to X" do
+      @roman.convert("X").should == 10
     end
 
 
