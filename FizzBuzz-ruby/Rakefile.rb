915edc08fc6a7b6905d7292f904f50691a6d07e9
mhouse89 <matthew.house89@gmail.com>
2015-12-11 16:27:27 -0500

    Cleaned up the Rakefile a bit after I messed around with the syntax a little.

--- FizzBuzz-ruby/Rakefile.rb
+++ FizzBuzz-ruby/Rakefile.rb
@@ -2,15 +2,13 @@ task :default => [:spec]
 
 begin
   require 'rspec/core/rake_task'
-  RSpec::Core::RakeTask.new(:spec)
+  RSpec::Core::RakeTask.new(:spec) do |t|
+    t.rspec_opts = '--format html --out reports/rspec_results.html'
+  end
 rescue LoadError
 end
 
-require 'rspec/core/rake_task'
 
-RSpec::Core::RakeTask.new(:spec) do |t|
-  t.rspec_opts = '--format html --out reports/rspec_results.html'
-end
 
 namespace :rspec_report do
   desc 'Run all specs and generate RSpec report in HTML'
