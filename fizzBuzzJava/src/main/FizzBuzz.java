380a9142b8bf5e0b8027493e30af0ea425679f57
Code Race 2 <coderace@Pillartechnologys-MacBook-Air.local>
2016-01-05 17:05:42 -0500

    Made changes modify the the tests to java

---
+++ fizzBuzzJava/src/main/FizzBuzz.java
@@ -0,0 +1,9 @@
+package kataBattle.fizzbuzz;
+
+class FizzBuzz {
+
+  public static String process(int input) {
+    return "";
+  }
+
+}
---
+++ fizzBuzzJava/src/test/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package kataBattle.fizzbuzz;
+
+import org.junit.Test;
+
+class FizzBuzzTest {
+
+  @Test
+  void returnsTheNumberThatItIsHanded() {
+    assertTrue(FizzBuzz.process(1) == "1");
+  }
+
+}
